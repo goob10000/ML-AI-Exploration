7f216c6b0b2a864b1dce2e6a5d9b463fe948d6c5
goob10000 <65315624+goob10000@users.noreply.github.com>
2025-02-28 17:29:08 -0800

    added y/m/d/t columns

--- first_test_space.py
+++ first_test_space.py
@@ -4,5 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 df = pl.read_csv("ML-AI-Exploration\dataexport_20250301T010932.csv",skip_rows=9)
-df.write_parquet("SFO_weather_data_2008to2025.parquet")
+# df.write_parquet("SFO_weather_data_2008to2025.parquet")
+df = pl.read_parquet("SFO_weather_data_2008to2025.parquet")
+df.insert_column(1, df['timestamp'].str.slice(0,4).alias("year"))
+df.insert_column(1, df['timestamp'].str.slice(4,2).alias("month"))
+df.insert_column(1, df['timestamp'].str.slice(6,2).alias("day"))
+df.insert_column(1, df['timestamp'].str.slice(9,4).alias("time"))
+df["time"]
+
 df
